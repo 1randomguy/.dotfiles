c68d9fcfe1cc24001759fa07b9146ef06ccf0a53
Benedikt <bblomberg123@gmail.com>
2024-05-17 12:00:50 +0200

    changed to zinit

--- setup.sh
+++ setup.sh
@@ -4,7 +4,29 @@
 git submodule init
 git submodule update
 
-base=(
+# check if root
+#if ! [[ ${EUID} -eq 0 ]] && [[ ${LIST_DEP} -eq 0 ]] ; then
+#	  echo "Run setup script with root permissions!"
+#  exit 1
+#fi
+
+# install required programs
+REQ="zsh stow"
+
+if command -v apt > /dev/null; then
+	sudo apt install $REQ
+elif command -v dnf > /dev/null; then
+	sudo dnf install $REQ
+else
+	echo "Please install $REQ manually"
+fi
+
+# install font
+# mkdir -p ~/.local/share/fonts
+# cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.otf
+
+# all folders to include
+app_configs=(
     zsh
 )
 
@@ -22,10 +44,13 @@ echo ""
 echo "Stowing apps for user: ${whoami}"
 
 # install apps available
-for app in ${base[@]}; do
+for app in ${app_configs[@]}; do
     #stowit "${HOME}" $app 
     stow -v -R -t "$HOME" $app
 done
+
+# set zsh as standard shell
+chsh --shell /bin/zsh ${whoami}
 
 echo ""
 echo "##### ALL DONE"
